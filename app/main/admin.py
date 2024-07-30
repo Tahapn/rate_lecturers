a50e845ba0bf0ff8a534ee07e9e7b1af69936198
Tahapn <tahapn860@gmail.com>
2024-07-30 11:51:32 +0330

    add subject and lecturersubject models

--- app/main/admin.py
+++ app/main/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import Lecturer
+from .models import Lecturer, Subject, LecturerSubject
 # Register your models here.
 
 admin.site.register(Lecturer)
+admin.site.register(Subject)
+
+
+@admin.register(LecturerSubject)
+class LecturerSubjectAdmin(admin.ModelAdmin):
+    ordering = ['subject__name']
--- app/main/models.py
+++ app/main/models.py
@@ -1,3 +1,4 @@
+from typing import Iterable
 from django.db import models
 from django.utils.text import slugify
 # Create your models here.
@@ -19,3 +20,33 @@ class Lecturer(models.Model):
         if not self.slug:
             self.slug = slugify(self.first_name + ' ' + self.last_name)
         return super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.first_name + ' ' + self.last_name
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(unique=True, blank=True)
+
+    def save(self, *args, **kwargs) -> None:
+        if not self.slug:
+            self.slug = slugify(self.name)
+        return super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class LecturerSubject(models.Model):
+    lecturer = models.ForeignKey(Lecturer, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    slug = models.SlugField(unique=True, blank=True)
+
+    def save(self, *args, **kwargs) -> None:
+        if not self.slug:
+            self.slug = slugify(self.lecturer.slug + ' '+self.subject.slug)
+        return super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.slug
