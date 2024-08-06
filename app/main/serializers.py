c54d5e236dd8bb135582aaaed90679d61903b8ae
Tahapn <tahapn860@gmail.com>
2024-08-06 11:23:21 +0330

    add ratings count to subjects API

--- app/main/serializers.py
+++ app/main/serializers.py
@@ -1,16 +1,31 @@
+from django.db.models import Count
 from rest_framework import serializers
 from .models import Lecturer, LecturerSubject
 
 
 class LecturerSubjectSerializer(serializers.ModelSerializer):
+    ratings = serializers.SerializerMethodField()
+    average = serializers.DecimalField(max_digits=3, decimal_places=2)
+
     class Meta:
         model = LecturerSubject
-        fields = ['id', 'slug']
+        fields = ['id', 'slug', 'average', 'ratings']
+
+    def get_ratings(self, obj):
+        return LecturerSubject.objects.values('review__rate').filter(
+            lecturer=self.context['lecturer_pk'], pk=obj.pk).annotate(count=Count('review'))
+
+
+class SimpleLecturerSubjectSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = LecturerSubject
+        fields = ['id', 'slug',]
 
 
 class LecturerSerializer(serializers.ModelSerializer):
-    average_ratings = serializers.IntegerField()
-    subjects = LecturerSubjectSerializer(many=True)
+    average_ratings = serializers.DecimalField(max_digits=3, decimal_places=2)
+    subjects = SimpleLecturerSubjectSerializer(many=True)
 
     class Meta:
         model = Lecturer
--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.db.models import Avg
+from django.db.models import Avg, Count
 from rest_framework import generics
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from .models import Lecturer, LecturerSubject
@@ -20,11 +20,19 @@ class LecturerSubjectList(generics.ListAPIView):
     serializer_class = LecturerSubjectSerializer
 
     def get_queryset(self, *args, **kwargs):
-        return LecturerSubject.objects.filter(lecturer=self.kwargs['lecturer_pk'])
+        return LecturerSubject.objects.prefetch_related('review_set').filter(lecturer=self.kwargs['lecturer_pk']).annotate(average=Avg('review__rate'))
+
+    def get_serializer_context(self):
+        return {'lecturer_pk': self.kwargs['lecturer_pk']}
 
 
 class LecturerSubjectDetail(generics.RetrieveAPIView):
     serializer_class = LecturerSubjectSerializer
 
     def get_object(self):
-        return get_object_or_404(LecturerSubject, pk=self.kwargs['pk'], lecturer=self.kwargs['lecturer_pk'])
+        queryset = LecturerSubject.objects.filter(
+            pk=self.kwargs['pk'], lecturer=self.kwargs['lecturer_pk']).annotate(average=Avg('review__rate'))
+        return get_object_or_404(queryset)
+
+    def get_serializer_context(self):
+        return {'lecturer_pk': self.kwargs['lecturer_pk']}
